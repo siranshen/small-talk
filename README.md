95c0074de192c817dc1f83e9b73fa436600e6f95
shensiran <shensiran@meituan.com>
2023-07-02 12:53:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## SmallTalk
+
+SmallTalk is an open source AI Chat Application specifically designed for learning languages. It is my first Next.js app and still at a very early stage.
+
+## Getting Started
+
+Start the app simply by running the following command:
+
+```bash
+npm run dev
+```
---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import './globals.css'
+import { Metadata } from 'next';
+
+export const siteTitle : string = "SmallTalk";
+
+export const metadata : Metadata = {
+  title: siteTitle,
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}
