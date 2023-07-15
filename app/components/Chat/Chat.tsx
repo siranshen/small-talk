e0ebd1b1c80131e5e5e54c431a912b42c2e40241
shensiran <shensiran@meituan.com>
2023-07-15 14:58:03 +0800

    Log audio stream processing for debugging on mobile

--- app/components/Chat/Chat.tsx
+++ app/components/Chat/Chat.tsx
@@ -154,6 +154,7 @@ export default function Chat() {
           buffers.push(event.data.buffer)
           break
         case 'final':
+          console.log('Received event', event.data.type)
           pushStream.write(new Int16Array(event.data.outputChannel).buffer)
           break
       }
