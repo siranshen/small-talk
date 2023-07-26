5a2ba8c7abd39f58346700fc68ba8d54439964fc
shensiran <shensiran@meituan.com>
2023-07-26 10:15:25 +0800

    Add translate effect to Toast

--- app/[locale]/components/toast/Toast.tsx
+++ app/[locale]/components/toast/Toast.tsx
@@ -40,13 +40,13 @@ export function Toast({
   const [fadeOut, setFadeOut] = useState(false)
 
   useEffect(() => {
-    setTimeout(() => setFadeOut(true), duration - 600)
+    setTimeout(() => setFadeOut(true), duration - 300)
   }, [duration])
 
   return (
     <div
       className={`${
-        fadeOut ? 'opacity-0' : 'opacity-1'
+        fadeOut ? 'opacity-0 -translate-y-4' : 'opacity-1'
       } transition duration-300 absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-black/50 px-4 py-3 text-white text-center`}
       onAnimationEnd={() => {
         if (fadeOut) {
