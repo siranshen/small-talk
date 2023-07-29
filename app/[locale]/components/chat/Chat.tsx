469f5142d263585d6c28e1af6182845dc25f56e9
shensiran <shensiran@meituan.com>
2023-07-29 14:02:57 +0800

    Rename history to convo so as to avoid confusion

--- app/[locale]/components/chat/Chat.tsx
+++ app/[locale]/components/chat/Chat.tsx
@@ -15,11 +15,10 @@ const SAMPLE_RATE = 24000
 export default function Chat() {
   const i18n = useTranslations('Chat')
   const i18nCommon = useTranslations('Common')
-
   const [toasts, addToast, removeToast] = useToasts()
 
   const chatContainerRef = useRef<HTMLDivElement>(null)
-  const [history, setHistory] = useState<ChatMessage[]>([])
+  const [convo, setConvo] = useState<ChatMessage[]>([])
 
   const isAutoplayEnabled = useRef<boolean>(false)
   const audioContextRef = useRef<AudioContext | null>(null)
@@ -28,8 +27,8 @@ export default function Chat() {
   const [isConfiguringAudio, setConfiguringAudio] = useState<boolean>(false)
   const [isTranscribing, setTranscribing] = useState<boolean>(false)
   const [isStreaming, setStreaming] = useState<boolean>(false)
-  const [shouldShowAiText, setShowText] = useState<boolean>(true)
   const [isPlayingAudio, setPlayingAudio] = useState<boolean>(false)
+  const [shouldShowAiText, setShowText] = useState<boolean>(true)
 
   const isSafari = useCallback(() => {
     return navigator.userAgent.indexOf('Safari') !== -1 && navigator.userAgent.indexOf('Chrome') === -1
@@ -72,18 +71,19 @@ export default function Chat() {
     localStorage.setItem('shouldShowAiText', shouldShowAiText ? 'true' : 'false')
   }, [shouldShowAiText])
 
+  /* Scroll to bottom upon new message */
   useEffect(() => {
     if (!chatContainerRef.current) {
       return
     }
     chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight
-  }, [history, isTranscribing])
+  }, [convo, isTranscribing])
 
   /* Request LLM to generate response and then synthesize voice */
   const generateResponse = useCallback(
-    async (newHistory: ChatMessage[]) => {
+    async (newConvo: ChatMessage[]) => {
       setStreaming(true)
-      setHistory([...newHistory, new ChatMessage('', true, true)])
+      setConvo([...newConvo, new ChatMessage('', true, true)])
       const learningLanguage = LANGUAGES_MAP[localStorage.getItem(LEARNING_LANG_FIELD) ?? LANGUAGES[0].locale]
       const ssProcessor = (speechSynthesisTaskProcessorRef.current = new SpeechSynthesisTaskProcessor(
         audioContextRef.current as AudioContext,
@@ -98,7 +98,7 @@ export default function Chat() {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            messages: newHistory.slice(-8).map((msg) => msg.toGPTMessage()),
+            messages: newConvo.slice(-8).map((msg) => msg.toGPTMessage()),
             language: learningLanguage.name,
             speakerName: learningLanguage.voiceNames[0].name, // TODO customize
           }),
@@ -116,7 +116,7 @@ export default function Chat() {
         addToast(i18nCommon('error'))
         console.error('Error generating response', e)
         setStreaming(false)
-        setHistory([...newHistory]) // Remove the loading message
+        setConvo([...newConvo]) // Remove the loading message
         return
       }
 
@@ -136,7 +136,7 @@ export default function Chat() {
             ssProcessor.pushTask({ text: lastMessage.substring(lastPauseIndex, pauseIndex) })
             lastPauseIndex = pauseIndex + PAUSE_TOKEN.length
           }
-          setHistory([...newHistory, new ChatMessage(lastMessage, true, true)])
+          setConvo([...newConvo, new ChatMessage(lastMessage, true, true)])
         }
         ssProcessor.pushTask({ text: lastMessage.substring(lastPauseIndex) })
         setStreaming(false)
@@ -144,7 +144,7 @@ export default function Chat() {
         const audioBlob = await ssProcessor.exportAudio()
         const newAudioMessage = new AudioChatMessage(lastMessage, true, audioBlob)
         await newAudioMessage.loadAudioMetadata()
-        setHistory([...newHistory, newAudioMessage])
+        setConvo([...newConvo, newAudioMessage])
       } catch (e) {
         addToast(i18nCommon('error'))
         console.error('Error while reading LLM response', e)
@@ -166,11 +166,11 @@ export default function Chat() {
   const sendText = useCallback(
     async (message: string) => {
       const newMessage = new ChatMessage(message, false)
-      const newHistory = [...history, newMessage]
-      setHistory(newHistory)
-      await generateResponse(newHistory)
+      const newConvo = [...convo, newMessage]
+      setConvo(newConvo)
+      await generateResponse(newConvo)
     },
-    [history, generateResponse]
+    [convo, generateResponse]
   )
 
   const startRecording = useCallback(async () => {
@@ -237,11 +237,11 @@ export default function Chat() {
     if (lastMessage.trim()) {
       const newAudioMessage = new AudioChatMessage(lastMessage, false, audioBlob)
       await newAudioMessage.loadAudioMetadata()
-      const newHistory = [...history, newAudioMessage]
-      setHistory(newHistory)
-      await generateResponse(newHistory)
+      const newConvo = [...convo, newAudioMessage]
+      setConvo(newConvo)
+      await generateResponse(newConvo)
     }
-  }, [addToast, generateResponse, history, i18nCommon])
+  }, [addToast, generateResponse, convo, i18nCommon])
 
   return (
     /* overflow-hidden prevents sticky div from jumping */
@@ -255,7 +255,7 @@ export default function Chat() {
       <div className='my-0 mx-auto h-full overflow-scroll' ref={chatContainerRef}>
         <div className='h-full'>
           <div className='max-w-[650px] my-0 mx-auto p-3'>
-            {history.map((msg) => (
+            {convo.map((msg) => (
               <ChatLineGroup key={msg.getId()} message={msg} shouldShowAiText={shouldShowAiText} />
             ))}
             {isTranscribing && <LoadingChatLineGroup isAi={false} />}
